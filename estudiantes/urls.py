ffd5a126600fa4a526467279b08027643fcc189e
gabgom2 <236346865+gabgom2@users.noreply.github.com>
2025-10-30 17:21:37 -0300

    v 0.1e / Completada botonera estudiantes con CRUD

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import estudiante_listado, estudiante_registro, estudiante_eliminar
+from .views import estudiante_listado, estudiante_registro, estudiante_eliminar, estudiante_editar, estudiante_detalle
 
 urlpatterns = [
     path("estlist/", estudiante_listado, name="estudiantelistado"),
     path("estreg/", estudiante_registro, name="estudianteregistro"),
-    path("<int:dni>/eliminar", estudiante_eliminar, name="estudianteeliminar")
+    path("<int:dni>/eliminar", estudiante_eliminar, name="estudianteeliminar"),
+    path("<int:dni>/editar", estudiante_editar, name="estudianteeditar"),
+    path("<int:dni>/detalle", estudiante_detalle, name="estudiantedetalle"),
 ]
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -9,7 +9,7 @@ from .forms import *
 
 def estudiante_listado(request):
     query = request.GET.get('q', '').strip()
-    mensaje = None
+    
     if query:     #si se busca algo
         query_estudiante = Estudiante.objects.filter(apellido__icontains = query).order_by("nombre")
         #si no se encuentra la busqueda
@@ -18,9 +18,8 @@ def estudiante_listado(request):
             messages.warning(request, "No se encontraron estudiantes con ese apellido, mostrando todos los resultados.")
     else: 
         query_estudiante = Estudiante.objects.all().order_by("apellido")
-        
-    
-    contexto = {"query": query, "query_estudiante": query_estudiante, "mensaje": mensaje}
+            
+    contexto = {"query": query, "query_estudiante": query_estudiante}
     return render(request, "estudiantes/estudiantelistado.html", contexto)
 
 #FORM estudiantes
@@ -35,7 +34,8 @@ def estudiante_registro(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Estudiante registrado/a con éxito.")
-            form = EstudianteForm()  # limpiar el formulario       
+            return redirect("estudiantelistado")
+            #form = EstudianteForm()  # limpiar el formulario
     else:
         form = EstudianteForm()    
       
@@ -46,3 +46,23 @@ def estudiante_eliminar(request, dni):
     estudiante.delete()
     messages.success(request, "Estudiante eliminado con éxito")
     return redirect("estudiantelistado")
+
+def estudiante_editar(request, dni):
+    estudiante = get_object_or_404(Estudiante, dni=dni)
+    if request.method == "POST":
+        form = EstudianteForm(request.POST, instance=estudiante)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Estudiante modificado con éxito.")
+            #form = EstudianteForm()  # limpiar el formulario       
+            return redirect("estudiantelistado")
+    else:        # IF request.method == GET / debería instanciar un formulario si lo voy a enviar por contexto
+        form = EstudianteForm(instance=estudiante)   
+    
+    contexto = {"form":form, "edicion":True}
+    return render(request, "estudiantes/estudianteregistro.html", contexto)
+
+def estudiante_detalle(request, dni):
+    estudiante = get_object_or_404(Estudiante, dni=dni)
+    return render(request, "estudiantes/estudiantedetalle.html", {"estudiante": estudiante})
+        
