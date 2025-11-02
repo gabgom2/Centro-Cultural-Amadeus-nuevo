fa290fd75d508d4d56df93348ca8e4ec8b1dd8ac
gabgom2 <236346865+gabgom2@users.noreply.github.com>
2025-11-02 19:44:28 -0300

    v 0.1n / Agregado template detalle con embed para partituras, agregado botón funcional de Detalles en listado partituras

--- partituras/urls.py
+++ partituras/urls.py
@@ -4,7 +4,8 @@ from .views import PartituraCreateView, PartituraDeleteView, PartituraDetailView
 
 urlpatterns = [path("", PartituraListView.as_view(), name="partituralistado"),
         path("partreg/", PartituraCreateView.as_view(), name="partituraregistro"),
-#        path("<str:codigo>/detalle", PartituraDetailView.as_view(), name=""),
+        path("<str:codigo>/detalle", PartituraDetailView.as_view(), name="partituradetalle"),
+        path("<str:codigo>/ver_pdf/", PartituraDetailView.ver_pdf_view, name="ver_pdf"),
 #        path("<str:codigo>/editar", PartituraUpdateView.as_view(), name=""),
 #        path("<str:codigo>/eliminar", PartituraDeleteView.as_view(), name=""),
  ]
--- partituras/views.py
+++ partituras/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib import messages
+from django.http import FileResponse, Http404
 from .models import Partitura
 from .forms import PartituraForm
+
 
 # Create your views here.
 
@@ -24,7 +26,27 @@ class PartituraCreateView(CreateView):
         return super().form_valid(form)
 
 class PartituraDetailView(DetailView):
-    pass
+    model = Partitura
+    template_name = "partituras/partituradetalle.html"
+    context_object_name = "partitura"
+    slug_field = "codigo"
+    slug_url_kwarg = "codigo"
+    
+    def ver_pdf(self, request):
+        partitura = self.get_object()
+        if not partitura.archivo:
+            raise Http404("Archivo no encontrado")
+
+        response = FileResponse(open(partitura.archivo.path, 'rb'), content_type='application/pdf')
+        response["X-Frame-Options"] = "SAMEORIGIN"
+        return response
+
+    @classmethod
+    def ver_pdf_view(cls, request, codigo):
+        """Método auxiliar para exponer ver_pdf como vista"""
+        self = cls()
+        self.kwargs = {'codigo': codigo}
+        return self.ver_pdf(request)
 
 class PartituraUpdateView(UpdateView):
     pass
