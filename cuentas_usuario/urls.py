1c3c2d17fa56825d2c5d3ff124be3910ef68b4f9
gabgom2 <236346865+gabgom2@users.noreply.github.com>
2025-11-03 17:43:24 -0300

    v 0.21c / Arreglado error en urls para login / logout

--- cuentas_usuario/urls.py
+++ cuentas_usuario/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import usuariodetalle, usuarioeditar, usuarioregistrar
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="cuentas_usuario/login.html"), name="usuariologin"),
-    path("logout/", LogoutView.as_view(template_name="cuentas_usuario/logout.html"), name="usuariologout"),
+    path("login/", LoginView.as_view(template_name="cuentas_usuario/usuariologin.html"), name="usuariologin"),
+    path("logout/", LogoutView.as_view(template_name="cuentas_usuario/usuariologout.html"), name="usuariologout"),
     path("detalle/", usuariodetalle, name="usuariodetalle"),
     path("editar/", usuarioeditar, name="usuarioeditar"),
     path("registrar/", usuarioregistrar, name="usuarioregistrar"),
